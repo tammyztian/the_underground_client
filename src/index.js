97014be70c025c79eaf316334203ef0b842c8d27
Tammy Tian <tiantammy@outlook.com>
2018-05-29 17:36:10 +0200

    removed extra initial files from bootstrap

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import {Provider} from 'react-redux';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import store from './store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const user = 
+  { name: 'Tammy',
+    weight: '115',
+    height: "5'4",
+    lifts:{
+      bench: 80,
+      deadlift: 225,
+      squat: 165
+    }
+};
+
+
+ReactDOM.render(
+<Provider store={store}>
+  <App user={user} />
+  </Provider>, 
+  document.getElementById('root'));
 registerServiceWorker();

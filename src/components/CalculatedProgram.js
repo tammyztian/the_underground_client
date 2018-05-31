8cac5ea9fcce7059033f4d4876a52af686282165
Tammy Tian <tiantammy@outlook.com>
2018-05-31 22:21:28 +0200

    made basic skeleton to render calculated program

---
+++ src/components/CalculatedProgram.js
@@ -0,0 +1,20 @@
+// import React from 'react';
+
+// export default function CalcualtedProgram(props) {
+
+
+
+//     return (
+//         <div className="form-group">
+//             <label htmlFor={props.id}>{props.label}</label>
+//             <output id={props.id} aria-live="polite">
+//                 ${props.value}
+//             </output>
+//         </div>
+//     );
+// }
+
+// Output.defaultProps = {
+//     value: 0
+// };
+

e71a2fb34b516210245d0065b4f78847a97eb3f7
Tammy Tian <tiantammy@outlook.com>
2018-06-01 20:29:19 +0200

    commented out for debugging

--- src/actions/utils.js
+++ src/actions/utils.js
@@ -1,17 +1,17 @@
-export const normalizeResponseErrors = res => {
-  if (!res.ok) {
-    if (
-      res.headers.has('Content-Type') && 
-      res.headers.get('Content-Type').startsWith('application/json')
-    ) {
-      return res.json()
-        .then(err => Promise.reject(err));
-    }
+// export const normalizeResponseErrors = res => {
+//   if (!res.ok) {
+//     if (
+//       res.headers.has('Content-Type') && 
+//       res.headers.get('Content-Type').startsWith('application/json')
+//     ) {
+//       return res.json()
+//         .then(err => Promise.reject(err));
+//     }
 
-    return Promise.reject({
-      code: res.status,
-      message: res.statusText
-    });
-  }
-  return res;
-}
+//     return Promise.reject({
+//       code: res.status,
+//       message: res.statusText
+//     });
+//   }
+//   return res;
+// }

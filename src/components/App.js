11e985157f52ccf76d85b36a20b6802b141e768e
Tammy Tian <tiantammy@outlook.com>
2018-06-01 20:29:53 +0200

    reverted landing page to be registration form (temp)

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,20 @@ import React from 'react';
 import './App.css';
 
 import RegistrationForm from './register-form.js';
+//import WeightForm from './lifting-experience';
+//import Dashboard from './Dashboard';
 
 export default function User(props){
  // const user = <p>Are you ready for your workout today {props.user.name}!?</p>;
-
+//<RegistrationForm />
   
     return (
-      <div className="App">
-        <h1> Welcome to the Underground </h1>
-        <RegistrationForm />
-      </div>
+        <div className="App">
+          <h1> Welcome to the Underground </h1>
+      
+          <RegistrationForm />
+
+        </div>
     );
   
 }

559cbdfdc2332d52c960bbc2f3336022d73a7659
Tammy Tian <tiantammy@outlook.com>
2018-06-01 20:34:41 +0200

    imported lifts reducer

--- src/store.js
+++ src/store.js
@@ -1,12 +1,16 @@
 import {createStore, applyMiddleware, combineReducers} from 'redux';
 import thunk from 'redux-thunk';
 import {reducer as formReducer} from 'redux-form';
+import {liftReducer} from './reducers/lifts';
 
 const store = createStore(
   combineReducers({
       form: formReducer,
+      lifts: liftReducer,
      
   }),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+
   applyMiddleware(thunk)
 );
 

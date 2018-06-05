8d296fb373de357e92bb97d99242885970b4fc65
Tammy Tian <tiantammy@outlook.com>
2018-06-05 20:45:26 +0200

    temp handle of day change

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,15 +1,69 @@
 import React from 'react';
 import Lifts from './lifts';
-import BenchDay1 from './IronSummerProgram/week1Bench';
+import SquatDay1 from '../IronSummerProgram/week1Squat';
+import BenchDay1 from '../IronSummerProgram/week1Bench';
+import DeadliftDay1 from '../IronSummerProgram/week1Deadlift';
 
 
-export default function Dashboard (){
+export function Dashboard (){
+    if (!this.props.auth) return <Redirect to="/" />;
+
+    if (this.props.day === 0) 
+        return 
+            <div className="Dashboard">
+                <Lifts /> 
+                <SquatDay1 />
+            </div>
+    
+    if (this.props.day === 1) 
+    return 
+        <div className="Dashboard">
+            <Lifts /> 
+            <BenchDay1 />
+        </div>
+
+    if (this.props.day === 2) 
+        return 
+            <div className="Dashboard">
+                <Lifts /> 
+                <DeadlifthDay1 />
+            </div>
+
     return (
           <div>
               <Lifts />
-              <BenchDay1 />
               
           </div>
       );
 }
 
+
+
+// i
+
+// export function LandingPage(props) {
+//     // If we are logged in redirect straight to the user's dashboard
+//     if (props.loggedIn) {
+//         return <Redirect to="/lifts" />;
+//     }
+
+//     return (
+//         <div className="home">
+//             <h2>Welcome to the Underground</h2>
+//             <LoginForm />
+//             <Link to="/register">Register</Link>
+//         </div>
+//     );
+// }
+
+const mapStateToProps = state => ({
+    auth: state.auth.authToken, 
+    user: state.auth.curretUser,
+    program: state.progarm.progarm,
+    day: state.program.day,
+    squat: state.lifts.squat,
+    loading: state.lifts.loading
+
+});
+
+export default connect(mapStateToProps)(Dashboard);

5ac2d614ea9510a370898708ad3fd9e13432f4a0
Tammy Tian <tiantammy@outlook.com>
2018-06-05 20:51:55 +0200

    created reducer to handle program day

---
+++ src/reducers/program.js
@@ -0,0 +1,30 @@
+import {GET_PROGRAM_SUCCESS, GET_PROGRAM_ERROR, GET_PROGRAM_REQUEST, PUT_PROGRAM_SUCCESS, PUT_PROGRAM_ERROR, PUT_PROGRAM_REQUEST} from '../actions/program';
+
+const initialState = {
+  program: null,
+  day: null,
+  loading: false,
+  error: null,
+  success: false,
+}
+
+export const programReducer = (state = initialState, action) => {
+  switch (action.type){
+    case GET_PROGRAM_REQUEST:
+      return{...state, loading: true, error: null, success: false}
+    case GET_PROGRAM_SUCCESS:
+      return { ...state, program: action.program, day:action.day, loading: false, success: true }
+    case GET_PROGRAM_ERROR:
+      return { ...state, loading: false, error: action.error }
+
+    case PUT_PROGRAM_REQUEST:
+      return { ...state,loading: true, error: null, success: false}
+    case PUT_PROGRAM_SUCCESS:
+      return { ...state, loading: false, success: true }
+    case PUT_PROGRAM_ERROR:
+      return { ...state, loading: false, error: action.error }
+
+    default:
+      return state
+  }
+}

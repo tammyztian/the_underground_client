472c2d3238fc2e3cc656130819796451ffd358d3
Tammy Tian <tiantammy@outlook.com>
2018-06-06 00:22:12 +0200

    added redirect, fixed js returns

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,65 +1,50 @@
 import React from 'react';
 import Lifts from './lifts';
+import {connect} from 'react-redux';
+import {Redirect} from 'react-router-dom';
+
 import SquatDay1 from '../IronSummerProgram/week1Squat';
 import BenchDay1 from '../IronSummerProgram/week1Bench';
 import DeadliftDay1 from '../IronSummerProgram/week1Deadlift';
 
+class Dashboard extends React.Component {
+   
+  
 
-export function Dashboard (){
-    if (!this.props.auth) return <Redirect to="/" />;
-
-    if (this.props.day === 0) 
-        return 
-            <div className="Dashboard">
+    render (){
+        if (this.props.authToken === null) return <Redirect to="/" />;
+   
+        if (this.props.day === 0) 
+            return <div className="dashboard">
+                    <Lifts /> 
+                    <SquatDay1 />
+                </div>
+        
+        if (this.props.day === 1) 
+        return  <div className="dashboard">
                 <Lifts /> 
-                <SquatDay1 />
-            </div>
-    
-    if (this.props.day === 1) 
-    return 
-        <div className="Dashboard">
-            <Lifts /> 
-            <BenchDay1 />
-        </div>
-
-    if (this.props.day === 2) 
-        return 
-            <div className="Dashboard">
-                <Lifts /> 
-                <DeadlifthDay1 />
+                <BenchDay1 />
             </div>
 
-    return (
-          <div>
-              <Lifts />
-              
-          </div>
-      );
+        if (this.props.day === 2) 
+            return <div className="dashboard">
+                    <Lifts /> 
+                    <DeadliftDay1 />
+                </div>
+
+        return (
+            <div>
+                <Lifts />
+                
+            </div>
+        );
+    };
 }
 
-
-
-// i
-
-// export function LandingPage(props) {
-//     // If we are logged in redirect straight to the user's dashboard
-//     if (props.loggedIn) {
-//         return <Redirect to="/lifts" />;
-//     }
-
-//     return (
-//         <div className="home">
-//             <h2>Welcome to the Underground</h2>
-//             <LoginForm />
-//             <Link to="/register">Register</Link>
-//         </div>
-//     );
-// }
-
 const mapStateToProps = state => ({
-    auth: state.auth.authToken, 
+    authToken: state.auth.authToken, 
     user: state.auth.curretUser,
-    program: state.progarm.progarm,
+    program: state.program.program,
     day: state.program.day,
     squat: state.lifts.squat,
     loading: state.lifts.loading

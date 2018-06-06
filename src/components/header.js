11422a16a2801e639cd8c20ea4612aecf2a78dad
Tammy Tian <tiantammy@outlook.com>
2018-06-06 14:28:17 +0200

    header

---
+++ src/components/header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../styles/header.css';
+
+export default function Header(props) {
+    
+    return (
+      <div class="jumbotron">
+        <div class="container">
+          <h1>Welcome to the Underground</h1>
+          <p> Iron summer 2018 is here. </p>
+        </div>
+      </div>
+    );
+}
+
+
+
+// export default connect(mapStateToProps)(LandingPage);

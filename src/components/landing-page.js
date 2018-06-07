a1e2a2f4d4e1daa33aa4c193e3cc49de8f3c5b41
Tammy Tian <tiantammy@outlook.com>
2018-06-07 22:56:54 +0200

    setting up for auth persistance

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -4,23 +4,27 @@ import {Link, Redirect} from 'react-router-dom';
 
 import LoginForm from './login-form';
 import Header from './header';
-export function LandingPage(props) {
-    // If we are logged in redirect straight to the user's dashboard
-    if (props.loggedIn) {
-        return <Redirect to="/lifts" />;
-    }
 
+export class LandingPage extends React.Component {
+    // If we are logged in redirect straight to the user's dashboard
+   
+    render (){
+        if (this.props.authToken) {
+            return <Redirect to="/lifts" />;
+        }
     return (
-        <div className="containter-fluid landing-page">
+        <div className="landing-page">
             <Header />
             <LoginForm />
             <Link to="/register">Register</Link>
         </div>
     );
+    }
 }
 
 const mapStateToProps = state => ({
-    loggedIn: state.auth.currentUser !== null
+    authToken: state.auth.authToken, 
+    user: state.auth.curretUser,
 });
 
 export default connect(mapStateToProps)(LandingPage);

15b5129478a860e489d5fc300a9270d187ba9d13
Tammy Tian <tiantammy@outlook.com>
2018-06-07 05:47:41 +0200

    added rough rendering based on day

--- src/components/lifts.js
+++ src/components/lifts.js
@@ -2,37 +2,77 @@ import React from 'react';
 import {connect} from 'react-redux';
 
 class Lifts extends React.Component {
-  submit = values => {
-    console.log(values);
-  }
-
 
   render() {
     if (this.props.loading){
       return <p> Loading </p>
     }
+
+    if (this.props.day === 0 || 3 || 6) {
       return (
         <div className="lifts-container">
-              <h1>Here are your entered records</h1>
+              <h1>Your max today:</h1>
             <div className='lifts'>
               <ul> 
                 <li> Bench {this.props.bench} </li>
-                <li> Squat {this.props.squat} </li>
-                <li> Deadlift {this.props.deadlift}</li>
               </ul>
           </div>
         </div>
       )
     }
+
+    if (this.props.day === 1 || 4 || 7){
+      return (
+        <div className="lifts-container">
+              <h1>Your max today:</h1>
+            <div className='lifts'>
+              <ul> 
+                <li> Squat {this.props.squat} </li>
+              </ul>
+          </div>
+        </div>
+      )
+    }
+
+    if (this.props.day === 2 || 5 || 8){return (
+      <div className="lifts-container">
+            <h1>Your max today:</h1>
+          <div className='lifts'>
+            <ul> 
+              <li> Deadlift {this.props.deadlift}</li>
+            </ul>
+        </div>
+      </div>
+    )}
+
+
+    // return (
+    //   <div className="lifts-container">
+    //         <h1>Here are your entered records</h1>
+    //       <div className='lifts'>
+    //         <ul> 
+    //           <li> Bench {this.props.bench} </li>
+    //           <li> Squat {this.props.squat} </li>
+    //           <li> Deadlift {this.props.deadlift}</li>
+    //         </ul>
+    //     </div>
+    //   </div>
+    // )
+  }
   }
   
 
 const mapStateToProps = state => ({
   authToken: state.auth.authToken, 
   user: state.auth.curretUser,
+
+  program: state.program.program,
+  day: state.program.day,
+
   bench: state.lifts.bench,
   squat: state.lifts.squat,
   deadlift: state.lifts.deadlift,
+
   loading: state.lifts.loading
 
 })

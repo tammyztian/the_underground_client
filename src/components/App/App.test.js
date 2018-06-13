ef05220e6090376bf1be45a317229151bf822f4e
Tammy Tian <tiantammy@outlook.com>
2018-06-13 21:47:46 +0200

    debugging deployment

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 
-import App from './app';
+import App from './App';
 
 describe('<App />', function (){
     it('renders without crashing', () => {
